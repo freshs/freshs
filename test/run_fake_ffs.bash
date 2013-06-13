4842714fdc634beb86d3c47f61a8f9f4cc96c3c5
Josh Berryman <the.real.josh.berryman@gmail.com>
2013-06-13 15:38:38 +0200

    added master script to run the ffs_fake test

---
+++ test/run_fake_ffs.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+###this is a master script to run the "fake" FFS test: it is "fake" because it uses trivial dynamics,
+###so it tests the server but not the client or any MD program.
+
+cd test_ffs_fake
+
+python main.py
+
+ 
+
