86f17444e674eaed20d0e725bab9efa34b8ac79b
Josh Berryman <the.real.josh.berryman@gmail.com>
2014-09-18 09:58:38 +0200

    Fixed cases in interface building script

--- scripts/buildLambdas.bash
+++ scripts/buildLambdas.bash
@@ -7,7 +7,7 @@ bin_width=0.01
 runs_per_bin=10
 
 ###print out a basic "Hypersurfaces" section for a control file
-echo "[Hypersurfaces]"
+echo "[hypersurfaces]"
 echo "lambdacount = $n_lambdas"
 echo "borderA     = $bin_min"
 lambda=$bin_min
@@ -22,7 +22,7 @@ echo
 echo
 
 ###print out a basic "Runs_per_interface" section for a control file
-echo "[Runs_per_interface]"
+echo "[runs_per_interface]"
 echo "borderA = $runs_per_bin"
 echo "borderB = $runs_per_bin"
 for i in `seq 1 $n_lambdas`
